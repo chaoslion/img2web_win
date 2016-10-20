95a2e9744012bc5ba67b024ff82b26ba65c69817
Alexander <trafo1@gmx.de>
2016-10-20 14:00:23 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace blogimage {
+    partial class frmMain {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnprocess = new System.Windows.Forms.Button();
+            this.clbblogposts = new System.Windows.Forms.CheckedListBox();
+            this.btnrefresh = new System.Windows.Forms.Button();
+            this.cball = new System.Windows.Forms.CheckBox();
+            this.lvDetails = new System.Windows.Forms.ListView();
+            this.lvDetailschFilename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvDetailschType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pbprocess = new System.Windows.Forms.ProgressBar();
+            this.lbprocess = new System.Windows.Forms.Label();
+            this.pythonProcess = new System.ComponentModel.BackgroundWorker();
+            this.lbprocessout = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnprocess
+            // 
+            this.btnprocess.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnprocess.Location = new System.Drawing.Point(404, 322);
+            this.btnprocess.Name = "btnprocess";
+            this.btnprocess.Size = new System.Drawing.Size(80, 25);
+            this.btnprocess.TabIndex = 1;
+            this.btnprocess.Text = "process";
+            this.btnprocess.UseVisualStyleBackColor = true;
+            this.btnprocess.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clbblogposts
+            // 
+            this.clbblogposts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.clbblogposts.FormattingEnabled = true;
+            this.clbblogposts.Location = new System.Drawing.Point(12, 17);
+            this.clbblogposts.Name = "clbblogposts";
+            this.clbblogposts.Size = new System.Drawing.Size(300, 379);
+            this.clbblogposts.TabIndex = 2;
+            this.clbblogposts.SelectedIndexChanged += new System.EventHandler(this.clbblogposts_SelectedIndexChanged);
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnrefresh.Location = new System.Drawing.Point(318, 322);
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(80, 25);
+            this.btnrefresh.TabIndex = 4;
+            this.btnrefresh.Text = "refresh";
+            this.btnrefresh.UseVisualStyleBackColor = true;
+            this.btnrefresh.Click += new System.EventHandler(this.btnrefresh_Click);
+            // 
+            // cball
+            // 
+            this.cball.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cball.AutoSize = true;
+            this.cball.Location = new System.Drawing.Point(318, 353);
+            this.cball.Name = "cball";
+            this.cball.Size = new System.Drawing.Size(69, 17);
+            this.cball.TabIndex = 5;
+            this.cball.Text = "check all";
+            this.cball.ThreeState = true;
+            this.cball.UseVisualStyleBackColor = true;
+            this.cball.Click += new System.EventHandler(this.cball_Click);
+            // 
+            // lvDetails
+            // 
+            this.lvDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvDetails.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvDetailschFilename,
+            this.lvDetailschType});
+            this.lvDetails.FullRowSelect = true;
+            this.lvDetails.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvDetails.Location = new System.Drawing.Point(318, 17);
+            this.lvDetails.MultiSelect = false;
+            this.lvDetails.Name = "lvDetails";
+            this.lvDetails.ShowGroups = false;
+            this.lvDetails.Size = new System.Drawing.Size(204, 299);
+            this.lvDetails.TabIndex = 6;
+            this.lvDetails.UseCompatibleStateImageBehavior = false;
+            this.lvDetails.View = System.Windows.Forms.View.Details;
+            // 
+            // lvDetailschFilename
+            // 
+            this.lvDetailschFilename.Text = "Filename";
+            this.lvDetailschFilename.Width = 91;
+            // 
+            // lvDetailschType
+            // 
+            this.lvDetailschType.Text = "Typ";
+            this.lvDetailschType.Width = 43;
+            // 
+            // pbprocess
+            // 
+            this.pbprocess.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbprocess.Location = new System.Drawing.Point(318, 372);
+            this.pbprocess.Name = "pbprocess";
+            this.pbprocess.Size = new System.Drawing.Size(204, 24);
+            this.pbprocess.Step = 1;
+            this.pbprocess.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbprocess.TabIndex = 7;
+            // 
+            // lbprocess
+            // 
+            this.lbprocess.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbprocess.AutoEllipsis = true;
+            this.lbprocess.AutoSize = true;
+            this.lbprocess.Location = new System.Drawing.Point(401, 354);
+            this.lbprocess.Name = "lbprocess";
+            this.lbprocess.Size = new System.Drawing.Size(22, 13);
+            this.lbprocess.TabIndex = 8;
+            this.lbprocess.Text = "foo";
+            // 
+            // pythonProcess
+            // 
+            this.pythonProcess.WorkerReportsProgress = true;
+            this.pythonProcess.DoWork += new System.ComponentModel.DoWorkEventHandler(this.pythonProcess_DoWork);
+            this.pythonProcess.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.pythonProcess_ProgressChanged);
+            this.pythonProcess.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.pythonProcess_RunWorkerCompleted);
+            // 
+            // lbprocessout
+            // 
+            this.lbprocessout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbprocessout.FormattingEnabled = true;
+            this.lbprocessout.Location = new System.Drawing.Point(12, 402);
+            this.lbprocessout.Name = "lbprocessout";
+            this.lbprocessout.Size = new System.Drawing.Size(510, 95);
+            this.lbprocessout.TabIndex = 9;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.lbprocessout);
+            this.Controls.Add(this.lbprocess);
+            this.Controls.Add(this.pbprocess);
+            this.Controls.Add(this.lvDetails);
+            this.Controls.Add(this.cball);
+            this.Controls.Add(this.btnrefresh);
+            this.Controls.Add(this.clbblogposts);
+            this.Controls.Add(this.btnprocess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(550, 550);
+            this.Name = "frmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "BlogImg2Web";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnprocess;
+        private System.Windows.Forms.CheckedListBox clbblogposts;
+        private System.Windows.Forms.Button btnrefresh;
+        private System.Windows.Forms.CheckBox cball;
+        private System.Windows.Forms.ListView lvDetails;
+        private System.Windows.Forms.ColumnHeader lvDetailschFilename;
+        private System.Windows.Forms.ColumnHeader lvDetailschType;
+        private System.Windows.Forms.ProgressBar pbprocess;
+        private System.Windows.Forms.Label lbprocess;
+        private System.ComponentModel.BackgroundWorker pythonProcess;
+        private System.Windows.Forms.ListBox lbprocessout;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace blogimage {
+    public partial class frmMain : Form {
+
+        // this should be configurable on runtime?!
+        const string PYTHON_PATH = @"C:\Python27\python.exe";
+        // do not change these as they reflect the www directory tree
+        const string SCRIPTS_FOLDER = @"..\";
+        const string BLOG_FOLDER = @"..\..\pages\02.blog\";
+
+        const string PARSE_OK = "ok";
+        const string PARSE_MISSING = "missing";
+        const string PARSE_EXCESS = "excess";
+        const string PARSE_OUTDATE = "outdated";
+
+        const int REPORT_PROCESS_INFO = 0;
+        const int REPORT_PROCESS_OUTPUT = 1;
+
+        private Dictionary<string, DirectoryInfo> blogposts;        
+        private string script_path, blog_path;
+
+        public frmMain() {
+            this.blogposts = new Dictionary<string, DirectoryInfo>();
+            string exepath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            this.script_path = Path.Combine(exepath, SCRIPTS_FOLDER);
+            this.blog_path = Path.Combine(exepath, BLOG_FOLDER);
+
+            InitializeComponent();            
+        }
+
+        // list missing images and old images
+        private bool mustProcessPost(DirectoryInfo post, out List<string> messages) {
+            messages = new List<string>();
+            List<string> valid_images = new List<string>();
+
+            FileInfo[] images = post.GetFiles();
+            FileInfo[] resimages = post.GetFiles("res/");
+            // filter only png and jpg
+            images = Array.FindAll(images, f => f.Name.ToLower().EndsWith(".jpg") || f.Name.ToLower().EndsWith(".png"));
+            resimages = Array.FindAll(resimages, f => f.Name.ToLower().EndsWith(".jpg") || f.Name.ToLower().EndsWith(".png"));
+            
+            if (images.Length != resimages.Length) {
+
+                // convert to string only arrays
+                List<string> images_names = new List<string>();
+                List<string> resimages_names = new List<string>();
+                foreach (FileInfo fi in images) {
+                    images_names.Add(fi.Name.ToLower());
+                }
+                foreach (FileInfo fi in resimages) {
+                    resimages_names.Add(fi.Name.ToLower());
+                }
+
+                // get same
+                var intersect = resimages_names.Intersect(images_names);
+
+                // log missing
+                foreach (string resimage in resimages_names) {                    
+                    if(!intersect.Contains(resimage)) {
+                        messages.Add(String.Format("{0}:{1}", PARSE_MISSING, resimage));
+                    }                    
+                }
+                // log excess
+                foreach (string image in images_names) {
+                    if (!intersect.Contains(image)) {
+                        messages.Add(String.Format("{0}:{1}", PARSE_EXCESS, image));
+                    }
+                }
+
+                valid_images.AddRange(intersect);
+            } else {
+                // lengths match -> compare write time
+                for (var i = 0; i < images.Length; i++) {
+                    FileInfo fi_image = images[i];
+                    FileInfo fi_resimage = resimages[i];
+                    if (fi_resimage.LastWriteTime > fi_image.LastWriteTime) {
+                        messages.Add(String.Format("{0}:{1}", PARSE_OUTDATE, fi_resimage.Name));
+                    } else {
+                        valid_images.Add(fi_image.Name);
+                    }
+                }
+            }
+
+            // add valid images
+            foreach(string image in valid_images) {
+                messages.Add(String.Format("{0}:{1}", PARSE_OK, image));
+            }
+
+            return (messages.Count - valid_images.Count) > 0;
+        }
+
+        private void UpdateBlogFolderListing() {
+            UseWaitCursor = true;
+            DirectoryInfo di = new DirectoryInfo(this.blog_path);
+
+            DirectoryInfo[] posts = di.GetDirectories();
+            // add to list
+            this.blogposts.Clear();
+            foreach (DirectoryInfo post in posts) {
+                this.blogposts.Add(post.Name, post);
+            }
+
+            // 1) sort by modified property            
+            Array.Sort(
+                posts,
+                delegate (DirectoryInfo x, DirectoryInfo y) {
+                    // get write time of blog_item.md files                    
+                    
+                    // i dont check for multiple md files or missing ones!
+                    FileInfo x_md = x.GetFiles("blog_item.md")[0];
+                    FileInfo y_md = y.GetFiles("blog_item.md")[0];
+                                        
+                    return x_md.LastWriteTime.CompareTo(y_md.LastWriteTime);
+            });
+            // sort descending
+            Array.Reverse(posts);
+
+            // 2) select all folder where:
+            // res/images are newer then /images
+            // size of res/images != /images
+            List<DirectoryInfo> post2check = new List<DirectoryInfo>();
+            List<DirectoryInfo> postnot2check = new List<DirectoryInfo>();
+            foreach (DirectoryInfo post in posts) {
+                List<string> messages;
+                if(mustProcessPost(post, out messages)) {
+                    post2check.Add(post);
+                } else {
+                    postnot2check.Add(post);
+                }
+            }
+
+            // add to listbox
+            this.clbblogposts.Items.Clear();
+            foreach (DirectoryInfo post in post2check) {
+                clbblogposts.Items.Add(post.Name, true);
+            }
+            foreach (DirectoryInfo post in postnot2check) {
+                clbblogposts.Items.Add(post.Name, false);
+            }
+            // set index
+            if(this.clbblogposts.Items.Count > 0 ) {
+                this.clbblogposts.SelectedIndex = 0;
+            }
+
+            // checkbox all
+            this.cball.ThreeState = true;            
+            this.cball.CheckState = CheckState.Indeterminate;
+            this.cball.Enabled = this.clbblogposts.Items.Count > 0;
+
+            // reset process ui
+            this.lbprocess.Text = "";
+            this.pbprocess.Value = 0;
+            UseWaitCursor = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // check if valid folder (contains blog.md)
+            DirectoryInfo di = new DirectoryInfo(this.blog_path);
+            if( di.GetFiles("blog.md").Length != 1 ) {
+                this.Close();
+                return;
+            }
+            UpdateBlogFolderListing();
+        }
+
+        private bool processPost(string post, out string output) {
+            string postpath = Path.Combine(this.blog_path, post);            
+            // remove old image files            
+            DirectoryInfo dipost = new DirectoryInfo(
+                postpath
+            );
+            FileInfo[] images = dipost.GetFiles();            
+            // filter only png and jpg
+            images = Array.FindAll(images, f => f.Name.EndsWith(".jpg") || f.Name.EndsWith(".png"));
+            foreach(FileInfo fiimg in images) {
+                fiimg.Delete();
+            }
+
+            // run img2web
+            Process p = new Process();            
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = PYTHON_PATH;
+            p.StartInfo.WorkingDirectory = this.script_path;
+            p.StartInfo.Arguments = String.Format(@"img2web\img2web.py {0}", postpath);
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            return p.ExitCode == 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {            
+            freezeWindowInputs();
+            lbprocessout.Items.Clear();
+            this.pbprocess.Maximum = this.clbblogposts.CheckedItems.Count;
+            this.pythonProcess.RunWorkerAsync();                        
+        }
+
+        private void clbblogposts_SelectedIndexChanged(object sender, EventArgs e) {
+            int idx = this.clbblogposts.SelectedIndex;
+            if (idx > -1) {
+                Dictionary<string, Color> details_typemap = new Dictionary<string, Color>();
+                details_typemap.Add(PARSE_OK, Color.LightGreen);
+                details_typemap.Add(PARSE_MISSING, Color.IndianRed);
+                details_typemap.Add(PARSE_EXCESS, Color.OrangeRed);
+                details_typemap.Add(PARSE_OUTDATE, Color.LightGreen);
+
+                lvDetails.Items.Clear();             
+                // get post string
+                string post = (string)this.clbblogposts.Items[idx];
+                // get post directory
+                DirectoryInfo postdir = this.blogposts[post];
+                List<string> messages;
+                this.mustProcessPost(postdir, out messages);
+                foreach(string msg in messages) {                    
+                    foreach (KeyValuePair<string, Color> dtm in details_typemap) {
+                        string header = String.Format("{0}:", dtm.Key);
+                        if (msg.StartsWith(header)) {
+                            string fname = msg.Substring(header.Length);
+                            ListViewItem lvitm = new ListViewItem(fname);
+                            lvitm.BackColor = dtm.Value;
+                            lvitm.SubItems.Add(dtm.Key);                            
+                            lvDetails.Items.Add(lvitm);
+                            break;
+                        }
+                    }                    
+                }
+                lvDetails.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                lvDetails.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            }
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e) {
+            freezeWindowInputs();
+            UpdateBlogFolderListing();
+            unfreezeWindowInputs();            
+        }
+        
+
+        private void pythonProcess_DoWork(object sender, DoWorkEventArgs e) {
+            // run img2web for all checked items
+            for (var i=0;i< this.clbblogposts.CheckedItems.Count;++i) {
+
+                string post = (string)this.clbblogposts.CheckedItems[i];
+                Tuple<int, string> info = new Tuple<int, string>(i + 1, post);
+                pythonProcess.ReportProgress(REPORT_PROCESS_INFO, info);
+
+
+                string output;                
+                if ( !processPost(post, out output) ) {
+                    i = this.clbblogposts.CheckedItems.Count-1;
+                }
+                string[] state = new string[2] { post, output };
+                pythonProcess.ReportProgress(REPORT_PROCESS_OUTPUT, output);
+            }
+        }
+
+        private void pythonProcess_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            if( e.ProgressPercentage == REPORT_PROCESS_INFO ) {
+                Tuple<int, string> info = (Tuple<int, string>)e.UserState;
+                this.lbprocess.Text = info.Item2;
+                this.pbprocess.Value = info.Item1;
+            } else {
+                string output = (string)e.UserState;
+                // add output seperator
+                lbprocessout.Items.Add("==================");
+                // split newlines
+                foreach (string line in Regex.Split(output, "\n")) {
+                    lbprocessout.Items.Add(line);
+                }
+            }            
+        }
+
+        void freezeWindowInputs() {
+            this.clbblogposts.Enabled = false;
+            this.btnprocess.Enabled = false;
+            this.btnrefresh.Enabled = false;         
+        }
+
+        void unfreezeWindowInputs() {
+            this.clbblogposts.Enabled = true;
+            this.btnprocess.Enabled = true;
+            this.btnrefresh.Enabled = true;
+        }
+
+        private void pythonProcess_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            UpdateBlogFolderListing();
+            unfreezeWindowInputs();
+        }
+
+        private void cball_Click(object sender, EventArgs e) {
+            this.cball.ThreeState = false;
+
+            if (this.cball.CheckState == CheckState.Checked) {                
+                for (var i = 0; i < this.clbblogposts.Items.Count; i++) {
+                    this.clbblogposts.SetItemChecked(i, true);
+                }
+            } else {                
+                for (var i = 0; i < this.clbblogposts.Items.Count; i++) {
+                    this.clbblogposts.SetItemChecked(i, false);
+                }
+            }
+        }
+    }
+}
